a06131dbd1bae56e9d1bdacaa40e3e9d923ed94b
Mageshsopra <122437601+Mageshsopra@users.noreply.github.com>
2023-03-09 16:52:29 +0530

    updated vpc

--- lambda-vpc/sample-vpc-environment-1/v1/infrastructure/vpc.tf
+++ lambda-vpc/sample-vpc-environment-1/v1/infrastructure/vpc.tf
@@ -1,26 +1,95 @@
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+resource "aws_vpc" "pubpvt-vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  #  instance_tenancy     = var.tenancy
 
-  name = var.environment.inputs.vpc_name
-  cidr = "10.0.0.0/16"
+  #    tags = {
+  #      Name = "${var.network_tag_prefix}-vpc"
+  #    }
+}
 
-  azs             = ["${var.aws_region}b"]
-  private_subnets = ["10.0.1.0/24"]
-  public_subnets  = ["10.0.101.0/24"]
-
-  enable_nat_gateway                             = true
-  enable_vpn_gateway                             = false
-  enable_ipv6                                    = true
-  assign_ipv6_address_on_creation                = true
-  private_subnet_assign_ipv6_address_on_creation = false
-  public_subnet_ipv6_prefixes                    = [0]
-  private_subnet_ipv6_prefixes                   = [1]
+#Creating 3 public subnets
+resource "aws_subnet" "master_sub" {
+  count                   = length(var.master_subnet_cidrs)
+  vpc_id                  = aws_vpc.pubpvt-vpc.id
+  cidr_block              = element(var.master_subnet_cidrs, count.index)
+  availability_zone       = element(var.azs, count.index)
+  map_public_ip_on_launch = true
+  depends_on              = [aws_internet_gateway.master] #Need to check
 
   tags = {
-    Terraform    = "true"
-    Environment  = var.environment.name
-    Provisioning = "via-proton"
-    Owner        = "Demo-EEM-TFC"
+    Name : "Master Subnet ${count.index + 1}"
   }
 }
 
+#Creating 3 private subnets
+resource "aws_subnet" "worker_sub" {
+  count             = length(var.worker_subnet_cidrs)
+  vpc_id            = aws_vpc.pubpvt-vpc.id
+  cidr_block        = element(var.worker_subnet_cidrs, count.index)
+  availability_zone = element(var.azs, count.index)
+  #depends_on              = [aws_nat_gateway.nat2]  #Need to check
+
+  tags = {
+    Name : "Worker Subnet ${count.index + 1}"
+  }
+}
+
+#Creating Internet Gateway
+resource "aws_internet_gateway" "master" {
+  vpc_id = aws_vpc.pubpvt-vpc.id
+
+  tags = {
+    Name = "Test_InternetGateway"
+  }
+}
+
+resource "aws_route_table" "master_rt" {
+  vpc_id = aws_vpc.pubpvt-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.master.id
+  }
+  tags = {
+    Name : "Master Route Table"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_asso" {
+  count          = length(var.master_subnet_cidrs)
+  subnet_id      = element(aws_subnet.master_sub[*].id, count.index)
+  route_table_id = aws_route_table.master_rt.id
+}
+
+resource "aws_eip" "eip" {
+  count = 3
+  vpc   = true
+
+}
+
+resource "aws_nat_gateway" "nat" {
+  count         = 3
+  allocation_id = element(aws_eip.eip[*].id, count.index)
+  subnet_id     = element(aws_subnet.master_sub[*].id, count.index)
+  tags = {
+    Name : "Nat GW ${count.index + 1}"
+  }
+}
+
+resource "aws_route_table" "worker_rt" {
+  count  = 3
+  vpc_id = aws_vpc.pubpvt-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = element(aws_nat_gateway.nat[*].id, count.index)
+  }
+  tags = {
+    Name : "Worker Route Table ${count.index + 1}"
+  }
+}
+
+resource "aws_route_table_association" "private_subnet_asso" {
+  count          = length(var.worker_subnet_cidrs)
+  subnet_id      = element(aws_subnet.worker_sub[*].id, count.index)
+  route_table_id = element(aws_route_table.worker_rt[*].id, count.index)
+}
