f972aad34289ae678ab0fef2fb249f86652d6352
Mageshsopra <122437601+Mageshsopra@users.noreply.github.com>
2023-03-09 16:34:26 +0530

    Update providers.tf

--- lambda-vpc/sample-vpc-environment-1/v1/infrastructure/providers.tf
+++ lambda-vpc/sample-vpc-environment-1/v1/infrastructure/providers.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.aws_region
   # alias  = "default"
   assume_role {
-    session_name = "eks-deploy-cross-account"
-    role_arn     = "arn:aws:iam::${var.environment.inputs.target_account}:role/eks-assume"
+    #session_name = "eks-deploy-cross-account"
+    role_arn     = "arn:aws:iam::842814951080:role/Magesh_Admin_Role"
   }
 }
