0ad513e373ec07a5dda18b46b9c3f726ff9de24a
Mageshsopra <122437601+Mageshsopra@users.noreply.github.com>
2023-03-10 16:35:12 +0530

    updated tag

--- lambda-vpc/sample-vpc-environment-1/v1/infrastructure/vpc.tf
+++ lambda-vpc/sample-vpc-environment-1/v1/infrastructure/vpc.tf
@@ -1,11 +1,11 @@
 resource "aws_vpc" "pubpvt-vpc" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
-  #  instance_tenancy     = var.tenancy
+   # instance_tenancy     = var.tenancy
 
-  #    tags = {
-  #      Name = "${var.network_tag_prefix}-vpc"
-  #    }
+      tags = {
+       Name = "${var.network_tag_prefix}-vpc"
+      }
 }
 
 #Creating 3 public subnets
