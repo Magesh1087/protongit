fc408e3eb9638568986b859829c546695d06cdf9
Mageshsopra <122437601+Mageshsopra@users.noreply.github.com>
2023-03-14 11:11:50 +0530

    Update providers.tf

--- lambda-vpc/sample-vpc-environment-1/v1/infrastructure/providers.tf
+++ lambda-vpc/sample-vpc-environment-1/v1/infrastructure/providers.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.aws_region
   # alias  = "default"
   assume_role {
-    #session_name = "eks-deploy-cross-account"
-    role_arn     = "arn:aws:iam::842814951080:role/ProtonCodebuildTest"
+    
+    role_arn     = "arn:aws:iam::842814951080:role/codebuild_Proton"
   }
 }
